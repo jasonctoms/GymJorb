0e6a40cd5592d8ec006cbabcf1f9ab431758e297
Jason Toms <jasonctoms@gmail.com>
2019-04-19 16:14:34 +0200

    Added logout and styled the bottom sheet

--- app/src/main/java/com/jorbital/gymjorb/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/jorbital/gymjorb/BottomNavigationDrawerFragment.kt
@@ -1,21 +1,23 @@
 package com.jorbital.gymjorb
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_bottomsheet.view.*
 
-class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+class BottomNavigationDrawerFragment : RoundedBottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         val v = inflater.inflate(R.layout.fragment_bottomsheet, container, false)
 
         v.navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.nav1 -> context!!.toast("Clicked sign out")
+                R.id.signOut -> signOut()
                 R.id.nav2 -> context!!.toast("Clicked settings")
                 R.id.nav3 -> context!!.toast("Clicked bug report")
             }
@@ -23,4 +25,15 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         }
         return v
     }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(context!!)
+            .addOnCompleteListener {
+                dismiss()
+                val main = activity as MainActivity
+                main.signedOutGoToLogin()
+            }
+    }
+
 }
--- app/src/main/java/com/jorbital/gymjorb/MainActivity.kt
+++ app/src/main/java/com/jorbital/gymjorb/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jorbital.gymjorb
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +30,9 @@ class MainActivity : AppCompatActivity() {
             bar.visibility = View.GONE
             fab.hide()
         }
+    }
+
+    fun signedOutGoToLogin() {
+        this.findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_loginFragment)
     }
 }
