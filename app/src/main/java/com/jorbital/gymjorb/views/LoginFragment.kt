a9ce5fafeea525a1c66e870119281c61f84db22b
Jason Toms <jasonctoms@gmail.com>
2019-04-22 19:08:51 +0200

    Update user object on login

--- app/src/main/java/com/jorbital/gymjorb/views/LoginFragment.kt
+++ app/src/main/java/com/jorbital/gymjorb/views/LoginFragment.kt
@@ -11,8 +11,11 @@ import androidx.navigation.fragment.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.jorbital.gymjorb.LoginFragmentDirections
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.jorbital.gymjorb.R
+import com.jorbital.gymjorb.data.User
+import timber.log.Timber
 
 private const val RC_SIGN_IN = 1
 
@@ -52,7 +55,8 @@ class LoginFragment : Fragment() {
 
             if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                val userId =user?.uid
+                updateUser(user)
+                val userId = user?.uid
                 val action =
                     LoginFragmentDirections.actionLoginFragmentToRoutinesFragment(userId)
                 this.findNavController().navigate(action)
@@ -61,9 +65,17 @@ class LoginFragment : Fragment() {
                 //TODO: user cancelled, decide what to do
                 else {
                     val error = response.error
+                    Timber.d(error)
                     //TODO handle this error
                 }
             }
         }
+    }
+
+    private fun updateUser(firebaseUser: FirebaseUser?) {
+        val userId = firebaseUser?.uid ?: ""
+        val user = User(firebaseUser?.displayName, userId)
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users").document(userId).set(user)
     }
 }
